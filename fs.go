656e5bafcd2047f6de912bbc2974bf6e01da549e
billcoding <wangmiao07@sina.com>
2024-01-12 17:21:26 +0800

    feat: finish

---
+++ fs.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+
+	"bytes"
+	"text/template"
+)
+
+//go:embed tpl.txt
+var fs string
+
+type (
+	Option struct {
+		ClassName        string
+		DartMapKeyType   string
+		DartMapValueType string
+		Fields           []*Field
+	}
+	Field struct {
+		JsonName  string
+		FieldName string
+		DataType  string
+		Default   string
+		Array     bool
+	}
+)
+
+func executeTpl(options []*Option) string {
+	var (
+		buf     = &bytes.Buffer{}
+		tpl     = template.Must(template.New("").Parse(fs))
+		err     error
+		results []string
+	)
+
+	for _, opt := range options {
+		buf.Reset()
+		if err = tpl.Execute(buf, &opt); err != nil {
+			panic(err.Error())
+		}
+		results = append(results, buf.String())
+	}
+
+	return strings.Join(results, "\n")
+}
